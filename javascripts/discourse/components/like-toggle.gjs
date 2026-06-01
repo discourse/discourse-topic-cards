discourse/discourse-topic-cards

commit ad60c74adbf701ad57d8acea4b6554f505c3efa9

DEV: Update linting (#73)

javascripts/discourse/components/like-toggle.gjs
@@ -4,27 +4,24 @@ import { on } from "@ember/modifier";
 import { action } from "@ember/object";
 import { debounce } from "@ember/runloop";
 import { service } from "@ember/service";
-import { not } from "truth-helpers";
 import concatClass from "discourse/helpers/concat-class";
 import icon from "discourse/helpers/d-icon";
 import number from "discourse/helpers/number";
 import { ajax } from "discourse/lib/ajax";
+import { resettableTracked } from "discourse/lib/tracked-tools";
+import { not } from "discourse/truth-helpers";
 import { i18n } from "discourse-i18n";
 
 export default class LikeToggle extends Component {
   @service dialog;
 
-  @tracked likeCount = this.args.topic.like_count;
-  @tracked liked = this.args.topic.op_liked || false;
+  @tracked liked = !!this.args.topic.op_liked;
   @tracked loading = false;
+  @resettableTracked likeCount = this.args.topic.like_count;
   clickCounter = 0;
 
-  get canLike() {
-    return this.args.topic.op_can_like || false;
-  }
-
   get firstPostId() {
-    return this.args.topic.first_post_id || false;
+    return !!this.args.topic.first_post_id;
   }
 
   @action
@@ -82,7 +79,7 @@ export default class LikeToggle extends Component {
       type="button"
       disabled={{not this.canLike}}
       title={{if
-        (this.canLike)
+        @topic.op_can_like
         (i18n (themePrefix "like_toggle.like"))
         (i18n (themePrefix "like_toggle.like_disabled"))
       }}

commit e3028c65b90ee3a73cf12fed266796726a47e901

FIX: likes were broken
ad60c74a broke two things:
- it removed canLike but did not replace the calls everywhere
- it turned firstPostId() into a boolean

javascripts/discourse/components/like-toggle.gjs
@@ -21,7 +21,7 @@ export default class LikeToggle extends Component {
   clickCounter = 0;
 
   get firstPostId() {
-    return !!this.args.topic.first_post_id;
+    return this.args.topic.first_post_id || false;
   }
 
   @action
@@ -77,7 +77,7 @@ export default class LikeToggle extends Component {
     <button
       {{on "click" this.toggleLikeDebounced}}
       type="button"
-      disabled={{not this.canLike}}
+      disabled={{not @topic.op_can_like}}
       title={{if
         @topic.op_can_like
         (i18n (themePrefix "like_toggle.like"))